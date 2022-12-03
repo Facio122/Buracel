f5b15b6048be78537278ddc91e59bff3dce7cada
Facio122 <piotkal487@student.polsl.pl>
2022-12-03 08:13:07 +0100

    big commit

--- Buracel/Assets/Scripts/PlayerMovementRolling.cs
+++ Buracel/Assets/Scripts/PlayerMovementRolling.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class PlayerMovementRolling : MonoBehaviour
 {
     private Rigidbody2D _rbPlayer;
+    private Transform _trPlayer;
     [SerializeField] private float _speedHorizontal = 1f;
+    [SerializeField] private float _speedRotate = 1f;
     void Start()
     {
         _rbPlayer= gameObject.GetComponent<Rigidbody2D>();
+        _trPlayer= gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
         _funMovement();
+        _funRollingRotate();
     }
 
     private void _funRollingRotate()
     {
-
+        float rotation = -90 * _speedRotate * _rbPlayer.velocity.x / 100;
+        _trPlayer.Rotate(new Vector3(0f, 0f, rotation));
     }
 
     private void _funMovement()
